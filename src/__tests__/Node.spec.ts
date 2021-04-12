d3542c24bbeb69378903f3ac55064da11a8f4185
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2021-04-12 18:03:30 +0200

    test(Node): Add tests for the _checkRI method

--- src/__tests__/Node.spec.ts
+++ src/__tests__/Node.spec.ts
@@ -9,5 +9,57 @@ describe('Node', () => {
 
             expect(node._checkRI(ancestors, successors)).toBe(true);
         });
+
+        it('should throw, if comparison with ancestors is incorrect', () => {
+            const node = new Node(6, 6);
+            const ancestors = [new Node(3, 3), new Node(8, 8)];
+            const successors = [new Node(7, 7), new Node(10, 10)];
+
+            expect(() => node._checkRI(ancestors, successors)).toThrow();
+        });
+
+        it('should throw, if comparison with successors is incorrect', () => {
+            const node = new Node(6, 6);
+            const ancestors = [new Node(3, 3), new Node(5, 5)];
+            const successors = [new Node(4, 4), new Node(10, 10)];
+
+            expect(() => node._checkRI(ancestors, successors)).toThrow();
+        });
+
+        it('should throw, if left child\'s parent is not the node', () => {
+            const node = new Node(6, 6);
+            node.left = new Node(5, 5);
+
+            expect(() => node._checkRI([], [])).toThrow();
+        });
+
+        it('should throw, if right child\'s parent is not the node', () => {
+            const node = new Node(6, 6);
+            node.right = new Node(7, 7);
+
+            expect(() => node._checkRI([], [])).toThrow();
+        });
+
+        it('should call children\'s _checkRI methods', () => {
+            const node = new Node(6, 6);
+            const leftChild = new Node(1, 1);
+            leftChild.parent = node;
+            node.left = leftChild;
+            const rightChild = new Node(15, 15);
+            rightChild.parent = node;
+            node.right = rightChild;
+            leftChild._checkRI = jest.fn(() => true);
+            rightChild._checkRI = jest.fn(() => true);
+
+            const ancestors = [new Node(3, 3), new Node(5, 5)];
+            const successors = [new Node(7, 7), new Node(10, 10)];
+
+            node._checkRI(ancestors, successors);
+
+            expect((leftChild._checkRI as jest.Mock).mock.calls[0][0]).toEqual(ancestors);
+            expect((leftChild._checkRI as jest.Mock).mock.calls[0][1]).toEqual([...successors, node]);
+            expect((rightChild._checkRI as jest.Mock).mock.calls[0][0]).toEqual([...ancestors, node]);
+            expect((rightChild._checkRI as jest.Mock).mock.calls[0][1]).toEqual(successors);
+        });
     });
 });
