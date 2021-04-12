91054c15ca232ce782bf3eb08b7166b7072abaf7
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2021-04-12 17:16:03 +0200

    test(BST): Add test for _checkRI method

--- src/__tests__/BST.spec.ts
+++ src/__tests__/BST.spec.ts
@@ -6,7 +6,7 @@ describe('BST', () => {
         it('should insert nodes into the tree while maintaining size and representation invariant', () => {
             const tree = new BST();
             expect(tree.size).toBe(0);
-            tree.insert(new Node(23, 23))
+            tree.insert(new Node(23, 23));
             expect(tree.size).toBe(1);
             tree.insert(new Node(8, 8));
             expect(tree.size).toBe(2);
@@ -26,6 +26,42 @@ describe('BST', () => {
             expect(tree.size).toBe(9);
 
             expect(tree._checkRI()).toBe(true);
+        });
+    });
+
+    describe('_checkRI', () => {
+        it('should return true, if representation invariant is valid', () => {
+            const tree = new BST();
+            tree.insert(new Node(23, 23));
+            tree.insert(new Node(8, 8));
+            tree.insert(new Node(4, 4));
+            tree.insert(new Node(16, 16));
+            tree.insert(new Node(15, 15));
+            tree.insert(new Node(17, 17));
+            tree.insert(new Node(18, 18));
+            tree.insert(new Node(42, 42));
+            tree.insert(new Node(33, 33));
+
+            expect(tree._checkRI()).toBe(true);
+        });
+
+        it('should throw, if representation invariant is invalid', () => {
+            const tree = new BST();
+            tree.insert(new Node(23, 23));
+            tree.insert(new Node(8, 8));
+            const brokenNodeParent = new Node(4, 4);
+            tree.insert(brokenNodeParent);
+            tree.insert(new Node(16, 16));
+            const brokenNode = new Node(15, 15);
+            tree.insert(brokenNode);
+            tree.insert(new Node(17, 17));
+            tree.insert(new Node(18, 18));
+            tree.insert(new Node(42, 42));
+            tree.insert(new Node(33, 33));
+            brokenNodeParent.right = brokenNode;
+            brokenNode.parent = brokenNodeParent;
+
+            expect(tree._checkRI.bind(tree)).toThrow();
         });
     });
 });
