f2c6d0577e0b4582175773a531ca33f5470d0899
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2021-04-12 17:25:20 +0200

    test(BST): Define the createTestTree util

--- src/__tests__/BST.spec.ts
+++ src/__tests__/BST.spec.ts
@@ -1,38 +1,32 @@
 import { BST } from '../BST';
 import { Node } from '../Node';
 
+const createTestTree = () => {
+    const tree = new BST();
+    tree.insert(new Node(23, 23));
+    tree.insert(new Node(8, 8));
+    tree.insert(new Node(4, 4));
+    tree.insert(new Node(16, 16));
+    tree.insert(new Node(15, 15));
+    tree.insert(new Node(17, 17));
+    tree.insert(new Node(18, 18));
+    tree.insert(new Node(42, 42));
+    tree.insert(new Node(33, 33));
+    return tree;
+};
+
 describe('BST', () => {
     describe('insert', () => {
         it('should insert nodes into the tree while maintaining size and representation invariant', () => {
-            const tree = new BST();
-            tree.insert(new Node(23, 23));
-            tree.insert(new Node(8, 8));
-            tree.insert(new Node(4, 4));
-            tree.insert(new Node(16, 16));
-            tree.insert(new Node(15, 15));
-            tree.insert(new Node(17, 17));
-            tree.insert(new Node(18, 18));
-            tree.insert(new Node(42, 42));
-            tree.insert(new Node(33, 33));
+            const tree = createTestTree();
             expect(tree.size).toBe(9);
-
             expect(tree._checkRI()).toBe(true);
         });
     });
 
     describe('_checkRI', () => {
         it('should return true, if representation invariant is valid', () => {
-            const tree = new BST();
-            tree.insert(new Node(23, 23));
-            tree.insert(new Node(8, 8));
-            tree.insert(new Node(4, 4));
-            tree.insert(new Node(16, 16));
-            tree.insert(new Node(15, 15));
-            tree.insert(new Node(17, 17));
-            tree.insert(new Node(18, 18));
-            tree.insert(new Node(42, 42));
-            tree.insert(new Node(33, 33));
-
+            const tree = createTestTree();
             expect(tree._checkRI()).toBe(true);
         });
 
